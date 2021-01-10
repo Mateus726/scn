2fe5d93017f6b91a23fd382905876c2223dc4cb2
Mateusu726 <Mateusfigueiredo083@gmail.com>
2021-01-10 15:12:28 -0400

    first commit

---
+++ src/help1.js
@@ -0,0 +1,34 @@
+const help1 = (prefix) => {
+
+	return `
+╔══✪〘 INFO 〙✪══
+║
+╠➥ MATEUS𝐁𝐎𝐓
+╠➥ *3.0*
+╠➥ 𝐃𝐎𝐍𝐎:  ⃬⃗MATEUS  ☔
+╠➥ *wa.me/+5566992007465
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠══✪〘 MENU1 〙✪══
+║
+╠➥ *${prefix}dono*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+║
+╠══✪〘 *FIM* 〙✪══
+
+════════════════════
+*MATEUS YT* 🤗
+*Digite ${prefix}dono para mais info*
+════════════════════`
+}
+exports.help1 = help1
+
